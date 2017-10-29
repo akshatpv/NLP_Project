a46e602f3128b8e992a7bd5875f6c9992bf825c3
aksha <aksha@AKSHATS-PC>
2017-10-30 00:13:45 +0530

    Add project files.

---
+++ Viterbi/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Viterbi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 82);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(626, 154);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ".";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 391);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = ".";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(53, 285);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 69);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(53, 408);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(35, 13);
+            this.status.TabIndex = 7;
+            this.status.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 432);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ Viterbi/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Viterbi
+{
+    public partial class Form1 : Form
+    {
+        const int num1 = 4, num2 = 5;
+        public static string[] tags = { "VB", "TO", "NN", "PPSS" };
+        public static string[] corpus = { "I", "want", "to", "race" };
+        Dictionary<string, Dictionary<string, double>> transition = new Dictionary<string, Dictionary<string, double>>();
+        Dictionary<string, Dictionary<string, double>> emission = new Dictionary<string, Dictionary<string, double>>();
+        Dictionary<string, double> viterbi = new Dictionary<string, double>();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            populateDictFromFiles();
+
+        }
+
+        private void populateDictFromFiles()
+        {
+            emission.Clear();
+            transition.Clear();
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(@"../../App_Data/processed_brown/transition.txt", System.Text.Encoding.Default))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadLine();
+                    String[] tags = line.Split(' ');
+                    foreach (String prior in tags)
+                    {
+                        String probabilityLine = sr.ReadLine();
+                        status.Text = probabilityLine;
+                        String[] probabilities = probabilityLine.Split(' ');
+                        Dictionary<string, double> priorTransition = new Dictionary<string, double>();
+                        for (int i = 0; i < tags.Length; i++)
+                        {
+                            priorTransition.Add(tags[i], float.Parse(probabilities[i]));
+                        }
+                        transition.Add(prior, priorTransition);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                status.Text = "The file could not be read:" + e.Message;
+            }
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(@"../../App_Data/processed_brown/emmission.txt", System.Text.Encoding.Default))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        status.Text = line;
+                        String[] tagWordsPair = line.Split('\t');
+                        String tag = tagWordsPair[0];
+
+                        Dictionary<string, double> emmissionDict = new Dictionary<string, double>();
+                        String words = tagWordsPair[1];
+                        String[] wordProbabilities = words.Split(' ');
+                        for(int i=0; i<wordProbabilities.Length; i += 2)
+                        {
+                            String wordForm = wordProbabilities[i];
+                            double probability = double.Parse(wordProbabilities[i+1]);
+                            emmissionDict.Add(wordForm, probability);
+                        }
+                        emission.Add(tag, emmissionDict);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                status.Text = "The file could not be read:" + e.Message;
+            }
+        }
+
+        private void populateDictFromStub()
+        {
+            emission.Clear();
+            Dictionary<string, double> tempEmissionArr = new Dictionary<string, double>();
+            tempEmissionArr.Add("I", 0);
+            tempEmissionArr.Add("want", 0.0093);
+            tempEmissionArr.Add("to", 0);
+            tempEmissionArr.Add("race", 0.00012);
+            emission.Add("VB", tempEmissionArr);
+
+            tempEmissionArr = new Dictionary<string, double>();
+            tempEmissionArr.Add("I", 0);
+            tempEmissionArr.Add("want", 0);
+            tempEmissionArr.Add("to", 0.99);
+            tempEmissionArr.Add("race", 0);
+            emission.Add("TO", tempEmissionArr);
+
+            tempEmissionArr = new Dictionary<string, double>();
+            tempEmissionArr.Add("I", 0);
+            tempEmissionArr.Add("want", 0.000054);
+            tempEmissionArr.Add("to", 0);
+            tempEmissionArr.Add("race", 0.00057);
+            emission.Add("NN", tempEmissionArr);
+
+            tempEmissionArr = new Dictionary<string, double>();
+            tempEmissionArr.Add("I", 0.37);
+            tempEmissionArr.Add("want", 0);
+            tempEmissionArr.Add("to", 0);
+            tempEmissionArr.Add("race", 0);
+            emission.Add("PPSS", tempEmissionArr);
+
+            transition.Clear();
+            Dictionary<string, double> tempTransitionArr = new Dictionary<string, double>();
+            tempTransitionArr.Add("VB", 0.019);
+            tempTransitionArr.Add("TO", 0.0043);
+            tempTransitionArr.Add("NN", 0.041);
+            tempTransitionArr.Add("PPSS", 0.067);
+            transition.Add("<s>", tempTransitionArr);
+
+            tempTransitionArr = new Dictionary<string, double>();
+            tempTransitionArr.Add("VB", 0.0038);
+            tempTransitionArr.Add("TO", 0.035);
+            tempTransitionArr.Add("NN", 0.047);
+            tempTransitionArr.Add("PPSS", 0.007);
+            transition.Add("VB", tempTransitionArr);
+
+            tempTransitionArr = new Dictionary<string, double>();
+            tempTransitionArr.Add("VB", 0.83);
+            tempTransitionArr.Add("TO", 0);
+            tempTransitionArr.Add("NN", 0.00047);
+            tempTransitionArr.Add("PPSS", 0);
+            transition.Add("TO", tempTransitionArr);
+
+            tempTransitionArr = new Dictionary<string, double>();
+            tempTransitionArr.Add("VB", 0.004);
+            tempTransitionArr.Add("TO", 0.16);
+            tempTransitionArr.Add("NN", 0.087);
+            tempTransitionArr.Add("PPSS", 0.0045);
+            transition.Add("NN", tempTransitionArr);
+
+            tempTransitionArr = new Dictionary<string, double>();
+            tempTransitionArr.Add("VB", 0.23);
+            tempTransitionArr.Add("TO", 0.00079);
+            tempTransitionArr.Add("NN", 0.0012);
+            tempTransitionArr.Add("PPSS", 0.00014);
+            transition.Add("PPSS", tempTransitionArr);
+        }
+
+        int first_char = 0;
+        string word = "", state = "``";
+        double prev_prob = 1, prob1, prob2;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text += listBox1.SelectedItem.ToString() + " ";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)   //backspacing doesnt work
+        {
+            int length = textBox1.Text.Length;
+            if (length > 0 && textBox1.Text[length - 1] == ' ')
+            {
+                first_char = textBox1.Text.LastIndexOf(' ', length - 2) + 1;
+                word = textBox1.Text.Substring(first_char, length - first_char).Trim();
+                label2.Text = "Current word: " + word;
+
+                label4.Text += ' ';
+                double max = 0;
+                //foreach (KeyValuePair<string, double> entry in transition[state]) //1st val of entry=< "<s>", VB >
+                for (int i = 0; i < transition[state].Count; i++)
+                {
+                    var entry = transition[state];
+                    prob1 = prev_prob * entry[tags[i]];
+                    try
+                    {
+                        prob2 = prob1 * emission[tags[i]][word];
+                    }
+                    catch (Exception exc)
+                    {
+                        prob2 = 0;
+                    }
+                    if (prob2 > max)
+                    {
+                        max = prob2;
+                        state = tags[i];
+                    }
+
+                }
+                prev_prob = max;
+                label4.Text += state;
+                string[] list_of_words = textBox1.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string next_best_word = predictNextWord(list_of_words, transition, emission);
+                listBox1.Items.Clear();
+                listBox1.Items.Add(next_best_word);
+            }
+            else if (length == 0)
+            {
+                word = "";
+                label2.Text = "Current word" + word;
+            }
+
+        }
+
+
+
+
+        public static string predictNextWord(string[] words, Dictionary<string, Dictionary<string, double>> transistion, Dictionary<string, Dictionary<string, double>> emission)
+        {
+            int MATRIX_ROWS = tags.Length + 2;
+            int MATRIX_COLUMNS = words.Length + 1;
+    
+            //Viterbi Matrix
+            double[,] matrix = new double[MATRIX_ROWS, MATRIX_COLUMNS];
+            bool firstWord = true;
+            int i = 0, j = 0;
+            foreach (string word in words)
+            {
+                //Initialization for the first word
+                if (firstWord)
+                {
+                    j = 1;
+                    foreach (string tag in tags)
+                    {
+                        matrix[j, i] = transistion["<s>"][tag] * emission[tag][word];
+                        j++;
+                    }
+                    firstWord = false;
+                    i++;
+                    continue;
+                }
+
+                //Viterbi algo
+                j = 1;
+                foreach (string tag in tags)
+                {
+                    double max = 0;
+                    int k = 1;
+                    foreach (string prevTag in tags)
+                    {
+                        double tagTransistion = matrix[k, i - 1] * transistion[prevTag][tag];
+                        if (tagTransistion > max)
+                        {
+                            max = tagTransistion;
+                        }
+                        k++;
+                    }
+                    matrix[j, i] = max * emission[tag][word];
+                    j++;
+                }
+                i++;
+
+            }
+
+            //trying all corpora and returning best match according to matrix found above
+            double highestProbability = 0;
+            string bestNextWord = "";
+            foreach (string corpora in corpus)
+            {
+                double highestProbabilityForCurrentCorpora = 0;
+                foreach (string tag in tags)
+                {
+                    int k = 1;
+                    double max = 0;
+                    foreach (string prevtag in tags)
+                    {
+                        double tagTransistion = matrix[k, i - 1] * transistion[prevtag][tag];
+                        if (tagTransistion > max)
+                        {
+                            max = tagTransistion;
+                        }
+                        k++;
+                    }
+                    double corporaProbabilityForCurrrentTag = max * emission[tag][corpora];
+                    if (corporaProbabilityForCurrrentTag > highestProbabilityForCurrentCorpora)
+                    {
+                        highestProbabilityForCurrentCorpora = corporaProbabilityForCurrrentTag;
+                    }
+                }
+
+                //If you want to return best three or whatever i just need to store this shit and sort
+                if (highestProbabilityForCurrentCorpora > highestProbability)
+                {
+                    highestProbability = highestProbabilityForCurrentCorpora;
+                    bestNextWord = corpora;
+                }
+            }
+            return bestNextWord;
+        }
+    }
+}
